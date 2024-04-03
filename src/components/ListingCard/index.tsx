657522bf870703c471da015fb73691e53626481f
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2024-04-03 12:20:20 -0300

    ListingCard searching for product-service

--- src/components/ListingCard/index.tsx
+++ src/components/ListingCard/index.tsx
@@ -1,34 +1,19 @@
 import "./styles.css";
+import * as productService from "../../services/product-service.ts";
 
 export default function ListingCard() {
   return (
     <main>
       <section className="container">
         <div className="card-listing">
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
-          <div className="card-listing-items">
-            <p className="listing-items-title">PC Gamer Pro</p>
-            <p className="listing-items-price">R$ 1200.00</p>
-          </div>
+          {productService.findByPrice(0, Number.MAX_VALUE).map((product) => (
+            <div key={product.id} className="card-listing-items">
+              <p className="listing-items-title">{product.name}</p>
+              <p className="listing-items-price">
+                R$ {product.price.toFixed(2)}
+              </p>
+            </div>
+          ))}
         </div>
       </section>
     </main>
